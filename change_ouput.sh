6c746c2537378af8070d9c07511e325490d33eb7
Davide Ficano <davide.ficano@gmail.com>
2014-07-06 16:39:25 +0200

    Fixed typo error on file name

--- change_ouput.sh
+++
@@ -1,5 +0,0 @@
-# Change the outputPath in config file subs.json
-# OUT_PATH must be an absolute path
-# The modified text file is written on stdout
-OUT_PATH=/home/
-cat test_env/subs.json | json_pp | ruby -e 'ARGF.each_line { |line| puts line.gsub(/(\s+"outputPath" : ")(.*)(".*)/, "\\1'${OUT_PATH}'\\3") }'
