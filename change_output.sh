9903addb720cbda67825f4c702559c7f250a7a60
Davide Ficano <davide.ficano@gmail.com>
2014-08-27 16:31:33 +0200

    Added script parameters

--- change_output.sh
+++ change_output.sh
@@ -1,5 +1,29 @@
 # Change the outputPath in config file subs.json
 # OUT_PATH must be an absolute path
 # The modified text file is written on stdout
-OUT_PATH=/home/
-cat test_env/subs.json | json_pp | ruby -e 'ARGF.each_line { |line| puts line.gsub(/(\s+"outputPath" : ")(.*)(".*)/, "\\1'${OUT_PATH}'\\3") }'
+
+usage() { echo "Usage: $0 [-c <config file>] [-n <new output path>]" 1>&2; exit 1; }
+
+while getopts ":c:n:" o; do
+    case "${o}" in
+        c)
+            CONFIG_PATH=${OPTARG}
+            ;;
+        n)
+            OUT_PATH=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${CONFIG_PATH}" ] || [ -z "${OUT_PATH}" ]; then
+    usage
+fi
+
+# example
+# ./change_output.sh -c test_env/subs.json -o /home/
+
+cat $CONFIG_PATH | json_pp | ruby -e 'ARGF.each_line { |line| puts line.gsub(/(\s+"outputPath" : ")(.*)(".*)/, "\\1'${OUT_PATH}'\\3") }'
