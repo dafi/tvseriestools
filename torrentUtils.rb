6566fee62ae2633ab3e01911d9639f700cff42e4
Davide Ficano <davide.ficano@gmail.com>
2015-09-25 08:19:09 +0200

    Follow links from the start to the destination page containing the final torrent link

--- torrentUtils.rb
+++ torrentUtils.rb
@@ -1,8 +1,15 @@
 class TorrentUtils
+#####
+## re The regular expression used to locate the link to torrent service
+## urlPattern The string to build to obtain the .torrent download link, it fills $1 with the part extracted from `re`
+## urlDestPage if present then the torrent service contains the download .torrent link inside this page, it fills $1 with the part extracted from `re`
+## reDestPage The regular expression used to locate the link to torrent service contained into the `urlDestPage`
+#####
+
     TorrentInfo = [
     {
     "re" => /href="http:\/\/www.torlock.com\/torrent\/([0-9]+)\/.*?"/,
-    "urlPattern" => 'http://www.torlock.com/tor/$1.torrent'
+    "urlPattern" => 'https://www.torlock.com/tor/$1.torrent'
     },
 
     {
@@ -24,6 +31,13 @@ class TorrentUtils
     "re" => /href="http:\/\/www\.bt-chat\.com\/details\.php\?id=([0-9]+).*?"/,
     "urlPattern" => 'http://www.bt-chat.com/download.php?id=$1'
     },
+
+    {
+    "re" => /href="(https:\/\/rarbg.com\/torrents\/.*?.html)"/,
+    "urlDestPage" => "$1",
+    "reDestPage" => /Click here to download torrent.* href="(.*?.torrent)"/,
+    "urlPattern" => 'https://rarbg.com$1'
+    },
     ]
 
     def self.getTorrentUrlFromFeedUrl(feedUrl)
@@ -33,7 +47,18 @@ class TorrentUtils
         TorrentInfo.index do |torrent|
             m = html.match(torrent["re"])
             if m && m.length == 2
-                torrentUrl = torrent["urlPattern"].gsub('$1', m[1])
+                urlDestPage = torrent["urlDestPage"]
+                if urlDestPage
+                    urlDestPage = urlDestPage.gsub('$1', m[1])
+                    htmlDestPage = open(urlDestPage).read.gsub(/(\n|\r|\t)/, '')
+                    mDest = htmlDestPage.match(torrent["reDestPage"])
+                    if mDest && mDest.length == 2
+                        torrentUrl = torrent["urlPattern"].gsub('$1', mDest[1])
+                        true
+                    end
+                else
+                    torrentUrl = torrent["urlPattern"].gsub('$1', m[1])
+                end
                 true
             end
         end
